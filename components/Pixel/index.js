705177e9fbd25b46af3cb2548121d3f03136a381
Ronaldo Lima <ron@ldinho.com.br>
2020-05-03 15:14:30 -0300

    Fix Pixel Facebook

---
+++ components/Pixel/index.js
@@ -0,0 +1,12 @@
+import Head  from 'next/head'
+
+import FACEBOOK_PIXEL_1 from '../analytics/pixel-1'
+
+export default ({name}) => {
+
+  return(
+    <Head>
+      {name === 'FACEBOOK_PIXEL_1' && <FACEBOOK_PIXEL_1 />}
+    </Head>
+  )
+}
