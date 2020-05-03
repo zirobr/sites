02417d56ea59c7b84b1b1f5c0b7c93649e08fd2e
Ronaldo Lima <ron@ldinho.com.br>
2020-05-03 18:16:36 -0300

    Pixel Fix 12

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/core/styles';
 import theme from '../src/theme';
+import Pixel from '../components/Pixel'
 
 export default class MyDocument extends Document {
   render() {
@@ -13,17 +14,7 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
-            <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s)
-  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}(window, document,'script',
-  'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '1008325152564716');
-  fbq('track', 'PageView');` }}
-    />
+          <Pixel />
         </Head>
         <body>
           <Main />
